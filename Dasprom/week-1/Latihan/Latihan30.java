f29d5416ed84b6dcd44911edbb8aebef9e58f2a0
TMTMPST <103508462+TMTMPST@users.noreply.github.com>
2023-09-01 08:24:56 +0700

    Add files via upload

---
+++ Dasprom/week-1/Latihan/Latihan30.java
@@ -0,0 +1,14 @@
+/**
+ * Latihan30
+ */
+public class Latihan30 {
+
+    public static void main(String[] args) {
+        System.out.println("**********Identitas Diri**********");
+        System.out.println("Nama \t: Vidi Joshubzky Saviola");
+        System.out.println("Absen \t: 30");
+        System.out.println("Kelas \t: 1A");
+        System.out.println("Prodi \t: D-IV Teknik Informatika");
+        System.out.println("Jurusan : Tekonogi Informasi");
+    }
+}
