c33a9af5f909283f3a437bec1273270247caf8c2
TMTMPST <vidoelag@gmail.com>
2023-09-11 13:25:30 +0700

    Kuliah

---
+++ Dasprom/week-3/ContohOperator30.java
@@ -0,0 +1,11 @@
+public class ContohOperator30 {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println("x++ = " + x++);
+        System.out.println("Setelah evaluasi, x = " + x);
+        x = 10;
+        System.out.println("++x = " + ++x);
+        System.out.println("Setelah evaluasi, x = " + x);
+        int y = 12;
+    }
+}
---
+++ Dasprom/week-3/ContohVariabel30.java
@@ -0,0 +1,18 @@
+/**
+ * ContohVariabel30
+ */
+public class ContohVariabel30 {
+
+    public static void main(String[] args) {
+        String hobby = "Bermain petak umpet";
+        boolean pandai = true;
+        char kelamin = 'L';
+        byte umur = 18;
+        double ipk = 3.58, tinggi = 1.65;
+        System.out.println(hobby);
+        System.out.println("Apakah pandai? " + pandai);
+        System.out.println("Jenis kelamin " + kelamin);
+        System.out.println("Umurku saat ini " + umur);
+        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", ipk, tinggi ));
+    }
+}
