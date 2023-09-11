a224473e2ffbce5319e9376334cddfa3a3cfbe77
TMTMPST <vidoelag@gmail.com>
2023-09-11 23:52:53 +0700

    week 3

--- Dasprom/week-3/Latihan30.java
+++ Dasprom/week-3/Latihan30.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
-public class Latihan {
+public class Latihan30 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         double r1, r2, r3, r4, I;
         float v;
 
         //Memasukkan Rangkaian Parrarel R1 - R3
-        System.out.println("Masukkan R1 : ");
+        System.out.print("Masukkan R1 : ");
         r1 = input.nextInt();
-        System.out.println("Masukkan R2 : ");
+        System.out.print("Masukkan R2 : ");
         r2 = input.nextInt();
-        System.out.println("Masukkan R3 : ");
+        System.out.print("Masukkan R3 : ");
         r3 = input.nextInt();
 
         //Hasil Rangkaian Parrarel R1 - R3
@@ -19,7 +19,7 @@ public class Latihan {
         System.out.println("Hasil Dari Rangkaian Pararel adalah " + Rp);
 
         //Memasukkan Rangkaian Seri R4
-        System.out.println("Masukkan R4 : ");
+        System.out.print("Masukkan R4 : ");
         r4 = input.nextInt();
 
         //Rumus Dari Rangkaian Gabungan antara Rangkaian Pararel dan R4
@@ -27,7 +27,7 @@ public class Latihan {
         System.out.println("Hasil Dari Rangkaian Rangkaian Gabungan adalah " + Rg);
 
         //Memasukan Arus Listrik
-        System.out.println("Masukkan I : ");
+        System.out.print("Masukkan I : ");
         I = input.nextInt();
 
         //Hasil Tegangan
