773e32395ed942d15b1fe4b8b99be7ada5cb4a20
TMTMPST <vidoelag@gmail.com>
2023-09-11 22:30:40 +0700

    week 3

---
+++ Dasprom/week-3/Latihan.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Latihan {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int r1, r2, r3, r4, I;
+        float V;
+
+        System.out.println("Masukkan R1 (pararel): ");
+        r1 = input.nextInt();
+        System.out.println("Masukkan R2(pararel): ");
+        r2 = input.nextInt();
+        System.out.println("Masukkan R3(pararel): ");
+        r3 = input.nextInt();
+        System.out.println("Masukkan R4: ");
+        r4 = input.nextInt();
+        System.out.println("Masukkan I: ");
+        I = input.nextInt();
+    }
+}
