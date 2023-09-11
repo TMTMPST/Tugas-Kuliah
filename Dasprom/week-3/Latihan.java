96fbda858a540a4328685324badd9b9209f0d438
TMTMPST <vidoelag@gmail.com>
2023-09-11 23:42:03 +0700

    week 3

--- Dasprom/week-3/Latihan.java
+++ Dasprom/week-3/Latihan.java
@@ -3,18 +3,35 @@ import java.util.Scanner;
 public class Latihan {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int r1, r2, r3, r4, I;
-        float V;
+        double r1, r2, r3, r4, I;
+        float v;
 
-        System.out.println("Masukkan R1 (pararel): ");
+        //Memasukkan Rangkaian Parrarel R1 - R3
+        System.out.println("Masukkan R1 : ");
         r1 = input.nextInt();
-        System.out.println("Masukkan R2(pararel): ");
+        System.out.println("Masukkan R2 : ");
         r2 = input.nextInt();
-        System.out.println("Masukkan R3(pararel): ");
+        System.out.println("Masukkan R3 : ");
         r3 = input.nextInt();
-        System.out.println("Masukkan R4: ");
+
+        //Hasil Rangkaian Parrarel R1 - R3
+        double Rp = 1 / (1 / r1 + 1 / r2 + 1 / r3);
+        System.out.println("Hasil Dari Rangkaian Pararel adalah " + Rp);
+
+        //Memasukkan Rangkaian Seri R4
+        System.out.println("Masukkan R4 : ");
         r4 = input.nextInt();
-        System.out.println("Masukkan I: ");
+
+        //Rumus Dari Rangkaian Gabungan antara Rangkaian Pararel dan R4
+        double Rg = Rp + r4;
+        System.out.println("Hasil Dari Rangkaian Rangkaian Gabungan adalah " + Rg);
+
+        //Memasukan Arus Listrik
+        System.out.println("Masukkan I : ");
         I = input.nextInt();
+
+        //Hasil Tegangan
+        v = (float) (I * Rg);
+        System.out.println("Tegangan V adalah : " + v + "volt"); 
     }
 }
