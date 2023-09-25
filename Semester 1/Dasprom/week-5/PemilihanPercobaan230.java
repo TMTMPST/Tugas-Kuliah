a3fdb46e12a185c8b2060798d0bbb9e995cdcccf
TMTMPST <vidoelag@gmail.com>
2023-09-25 10:24:38 +0700

    update

---
+++ Semester 1/Dasprom/week-5/PemilihanPercobaan230.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PemilihanPercobaan230 {
+    public static void main(String[] args) {
+        Scanner input30 = new Scanner(System.in);
+        System.out.print("Nilai UAS : ");
+        float uas = input30.nextFloat();
+        System.out.print("Nilai UTS : ");
+        float uts = input30.nextFloat();
+        System.out.print("Nilai Kuis : ");
+        float kuis = input30.nextFloat();
+        System.out.print("Nilai Tugas  : ");
+        float tugas = input30.nextFloat();
+
+        float total = (uas * 0.4f)
+    }
+}
