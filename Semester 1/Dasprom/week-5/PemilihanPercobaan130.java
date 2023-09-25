423e46b738fdcc7f82914d9e7ef5a4807bf57438
TMTMPST <vidoelag@gmail.com>
2023-09-25 13:24:12 +0700

    Update PemilihanPercobaan130.java

--- Semester 1/Dasprom/week-5/PemilihanPercobaan130.java
+++ Semester 1/Dasprom/week-5/PemilihanPercobaan130.java
@@ -6,10 +6,7 @@ public class PemilihanPercobaan130 {
         Scanner input30 = new Scanner(System.in);
         int angka = input30.nextInt();
         
-        if (angka%2 == 0) {
-            System.out.println("Angka " + angka + " bilangan genap");
-        } else {
-            System.out.println("Angka " + angka + " bilangan ganjil");
-    }
+        String hasil = (angka % 2 == 0) ? " Bilangan genap" : " Bilangan Ganjil";
+        System.out.println("Angka " + angka + hasil);
     }
 }
