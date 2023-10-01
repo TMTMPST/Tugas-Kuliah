fb2add3d259cb44200c3fca64a682d275843feb7
TMTMPST <vidoelag@gmail.com>
2023-10-01 19:49:59 +0700

    nj

---
+++ Semester 1/Dasprom/week-5/latihan2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class latihan2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String user, Pass;
+
+        System.out.print("Masukkan Username : ");
+        user = sc.nextLine();
+
+        System.out.print("Masukkan Pass: ");
+        pass = sc.nextLine();
+
+        if (user.equalsIgnoreCase(Pass))
+    }
+}
