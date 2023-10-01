9a802cdb9b342964b7fb687975cbc16c07b7f1e3
TMTMPST <vidoelag@gmail.com>
2023-10-01 19:47:51 +0700

    asd

--- Semester 1/Dasprom/week-5/PemilihanPercobaan230.java
+++ Semester 1/Dasprom/week-5/PemilihanPercobaan230.java
@@ -15,17 +15,17 @@ public class PemilihanPercobaan230 {
         float total = (uas * 0.4f) + (uts * 0.3f) + (kuis * 0.1f) + (tugas * 0.2f);
         if (80 <= total && total <= 100) {
             System.out.println(" Mendapatkan Nilai A (Sangat Baik)");
-        } else if (73 <= total && total < 80){
+        } if (73 <= total && total < 80){
             System.out.println("Mendapatkan Nilai  B+ (Lebih dari Baik)");
-        } else if (65 <= total && total < 73){
+        } if (65 <= total && total < 73){
             System.out.println("Mendapatkan Nilai B (Baik)");
-        } else if (60 <= total && total < 65) {
+        } if (60 <= total && total < 65) {
             System.out.println("Mendapatkan Nilai C+ (Lebih dari Cukup)");
-        } else if (50 <= total && total < 60) {
+        } if (50 <= total && total < 60) {
             System.out.println("Mendapatkan Nilai C (Cukup)");
-        } else if (39 <= total && total < 50) {
+        } if (39 <= total && total < 50) {
             System.out.println("Mendapatkan Nilai D (Kurang)");
-        } else if (total < 39) {
+        } if (total < 39) {
             System.out.println("Mendapatkan Nilai E (Gagal)");
         }
     }           
