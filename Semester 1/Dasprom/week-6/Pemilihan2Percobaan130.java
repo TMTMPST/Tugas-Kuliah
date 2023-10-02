bdf5631edd9be88c7150470b8d494b7ae39544e4
TMTMPST <vidoelag@gmail.com>
2023-10-02 13:41:30 +0700

    update

--- Semester 1/Dasprom/week-6/Pemilihan2Percobaan130.java
+++ Semester 1/Dasprom/week-6/Pemilihan2Percobaan130.java
@@ -9,7 +9,9 @@ public class Pemilihan2Percobaan130 {
         
         if ((thn % 4) == 0) {
             if ((thn % 100)!=0) {
-                System.out.println("Tahun Kabisat");
+                System.out.println(thn + " Tahun Kabisat");
+            } else if ((thn % 400)==0){
+                System.out.println(thn + " Tahun Kabisat");
             }
             else {
                 System.out.println("Bukan Tahun Kabisat");
