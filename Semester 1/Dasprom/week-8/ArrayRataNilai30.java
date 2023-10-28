a41b17e8219b2a8d1b447106fa3d61d19e704269
TMTMPST <vidoelag@gmail.com>
2023-10-28 17:00:26 +0700

    week 8

--- Semester 1/Dasprom/week-8/ArrayRataNilai30.java
+++ Semester 1/Dasprom/week-8/ArrayRataNilai30.java
@@ -4,17 +4,32 @@ public class ArrayRataNilai30 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
+        System.out.print("Masukkan Jumlah Mahasiswa : ");
+        int jmlMhs = sc.nextInt();
+        int[] nilaiMhs = new int[jmlMhs];
 
+        double totalLulus = 0 , totalTdkLulus = 0;
+        double rata2Lulus, rata2TdkLulus;
+        int counterlls = 0;
+        int counterTdkLls = 0;
+        
         for (int i = 0; i < nilaiMhs.length; i++) {
-            System.out.print("Masukkan Nilai Mahasiswa ke - " + (i+i) + " : ");
+            System.out.print("Masukkan Nilai Mahasiswa ke - " + (i+1) + " : ");
             nilaiMhs[i] = sc.nextInt();
         } for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                totalLulus += nilaiMhs[i];
+                counterlls++;
+            } else if (nilaiMhs[i] <= 70) {
+                totalTdkLulus += nilaiMhs[i];
+                counterTdkLls++;
+            }
         }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata - rata nilai = " + rata2);
+        System.out.println("Jumlah Mahasiswa yang lulus adalah : "  + counterlls);
+        rata2Lulus = totalLulus/counterlls;
+        System.out.println("Rata - rata nilai Lulus = " + rata2Lulus);
+        System.out.println("Jumlah Mahasiswa yang lulus adalah : "  + counterTdkLls);
+        rata2TdkLulus = totalTdkLulus/counterTdkLls;
+        System.out.println("Rata - rata nilai Lulus = " + rata2TdkLulus);
     }
 }
