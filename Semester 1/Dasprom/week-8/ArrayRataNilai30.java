bcc9552682aaee7de791ff43031515ea83b7fd1f
TMTMPST <vidoelag@gmail.com>
2023-10-28 16:11:28 +0700

    week 8

--- Semester 1/Dasprom/week-8/ArrayRataNilai30.java
+++ Semester 1/Dasprom/week-8/ArrayRataNilai30.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ArrayRataBilai30 {
+public class ArrayRataNilai30 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
