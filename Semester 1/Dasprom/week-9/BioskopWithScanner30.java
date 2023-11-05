a933587652ea052d8225e0034a2dfa90c196b9b1
TMTMPST <vidoelag@gmail.com>
2023-11-05 19:12:29 +0700

    week 10

--- Semester 1/Dasprom/week-9/BioskopWithScanner30.java
+++ Semester 1/Dasprom/week-9/BioskopWithScanner30.java
@@ -6,24 +6,70 @@ public class BioskopWithScanner30 {
         String nama, next;
         int baris, kolom;
         String[][] penonton = new String[4][2];
+        Boolean benar = true;
 
-        while (true) {
-            System.out.print("Masukkan Nama : ");
-            nama = sc.nextLine();
-            System.out.print("Masukkan Baris : ");
-            baris = sc.nextInt();
-            System.out.print("Masukkan Kolom : ");
-            kolom = sc.nextInt();
+        while (benar) {
+            int menu;
+            System.out.println("Pilih menu ");
+            System.out.println("- Menu 1 : Input data Penonton");
+            System.out.println("- Menu 2 : Tampilkan daftar penonton");
+            System.out.println("- Menu 3 : Exit");
+            System.out.print("Masukkan Menu : ");
+            menu = sc.nextInt();
+
             sc.nextLine();
 
-            penonton[baris-1][kolom-1] = nama;
+            if (menu == 1) {
+                do {
+                    
+                    
+                    System.out.print("Masukkan nama: ");
+                    nama = sc.nextLine();
+                    while (true) {
+                        System.out.print("Masukkan baris: ");
+                        baris = sc.nextInt();
+                        System.out.print("Masukkan kolom: ");
+                        kolom = sc.nextInt();
+                        sc.nextLine();
+                        
+                        if ((baris - 1) >= penonton.length || (kolom - 1) >= penonton[baris - 1].length) {
+                            System.out.println("Baris atau Kolom tidak tersedia\nMembuang Inputan");
+                        } else if (penonton[baris - 1][kolom - 1] == null){
+                            penonton[baris - 1][kolom - 1] = nama;
+                        }
+                        else {
+                            System.out.println("Kursi telah diisi.\nMohon masukkan kembali baris dan kolom");
+                            continue;
+                        }
+                        break;
+                    }
 
-            System.out.print("Input penonton lainnya  ? (y/n): ");
-            next = sc.nextLine();
+                        System.out.print("Input penonton lainnya? (y/n): ");
+                    next = sc.nextLine();
+                    
+                } while (next.equalsIgnoreCase("y"));
 
-            if (next.equalsIgnoreCase("n")) {
-                break;
+            } if (menu == 2) {
+                for (int i = 0; i < penonton.length; i++) {
+                    System.out.print("Penonton baris ke-" + i + ": ");
+                    Boolean isNext = false;
+                    for (String j : penonton[i]) {
+                        if (isNext)
+                        System.out.print(", ");
+                        System.out.print((j == null) ? "***" : j);
+                        isNext = true;
+                        }
+                        System.out.println();
+                    }
+                    continue;
+
+            } if (menu == 3) {
+                benar = false;
+
+            } else {
+                System.out.println("Input yang anda masukkan Invalid !!!!");
             }
         }
+        
     }
 }
