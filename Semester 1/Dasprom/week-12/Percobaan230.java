b20a66d7abb14fc261510d52a768416bd76ffab2
TMTMPST <vidoelag@gmail.com>
2023-11-27 18:06:48 +0700

    week 14

--- Semester 1/Dasprom/week-12/Percobaan230.java
+++ Semester 1/Dasprom/week-12/Percobaan230.java
@@ -8,14 +8,16 @@ public class Percobaan230 {
             return (x * hitungPangkat(x, y - 1));
         }
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Bilangan yang dihitung : ");
         int bilangan = sc.nextInt();
         System.out.print("Pangkat : ");
         int pangkat = sc.nextInt();
-
+        for (int i = 1; i <= pangkat; i++) {
+            System.out.print(bilangan + " x ");
+        }
+        System.out.print("1 = ");
         System.out.println(hitungPangkat(bilangan, pangkat));
     }
 }
---
+++ Semester 1/Dasprom/week-12/tugas330.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class tugas330 {
+    static boolean primaRekursif(int x) {
+        if (x <= 1) {
+            return false;
+        } else {
+            for (int i = 2; i < x; i++) {
+                if (x % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan Angka : ");
+        int N = sc.nextInt();
+
+        System.out.print(primaRekursif(N) ? "Benar" : "Salah");
+        
+    }
+}
