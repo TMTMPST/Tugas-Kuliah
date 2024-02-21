b7c2573842c4486f96697d420ad9c1ec8114ef34
Vidi Joshubzky Saviola <103508462+TMTMPST@users.noreply.github.com>
2024-02-21 21:05:29 +0700

    Create BukuMain30.java

---
+++ Semester2/Algoritma/D1/Buku30/BukuMain30.java
@@ -0,0 +1,16 @@
+public class BukuMain30 {
+    public static void main(String[] args) {
+        
+        Buku30 bk1 = new Buku30();
+        bk1.judul = "Today Ends Tommorow Comes";
+        bk1.pengarang = "Denanda pratiwi";
+        bk1.halaman = 198;
+        bk1.stok = 13;
+        bk1.harga = 71000;
+        
+        bk1.tampilInformasi();
+        bk1.terjual(5);
+        bk1.gantiHarga(60000);
+        bk1.tampilInformasi();
+    }
+}
