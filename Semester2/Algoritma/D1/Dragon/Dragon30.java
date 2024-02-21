0b62b495ec627f880d29e832825b758e284817ff
Vidi Joshubzky Saviola <103508462+TMTMPST@users.noreply.github.com>
2024-02-21 20:55:57 +0700

    weeee

---
+++ Semester2/Algoritma/D1/Dragon/Dragon30.java
@@ -0,0 +1,50 @@
+public class Dragon30 {
+
+    private int x; 
+    private int y;
+    private int width; 
+    private int height; 
+
+   public Dragon30 (int x, int y, int width, int height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+   }
+    void moveLeft() {
+        x--;
+        if (x < 0 || x > width) {
+            detectCollision();
+        }
+    }
+
+    void moveRight() {
+        x++;
+        if (x < 0 || x > width) {
+            detectCollision();
+        }
+    }
+
+    void moveUp() {
+        y--;
+        if (y < 0 || y > height) {
+            detectCollision();
+        }
+    }
+
+    void moveDown() {
+        y++;
+        if (y < 0 || y > height) {
+            detectCollision();
+        }
+    }
+
+
+    void printPosition() {
+        System.out.println("Posisi dragon: (" + x + ", " + y + ")");
+    }
+
+    void detectCollision() {
+        System.out.println("Game Over!");
+    }
+}
---
+++ Semester2/Algoritma/D1/Dragon/DragonMain30.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class DragonMain30 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukkan lebar area permainan: ");
+        int width = input.nextInt();
+        
+        System.out.print("Masukkan tinggi area permainan: ");
+        int height = input.nextInt();
+        
+        Dragon30 dragon1 = new Dragon30(0, 0, width, height);
+
+
+        while (true) {
+            dragon1.printPosition();
+            System.out.print("Masukkan arah gerakan (a/w/s/d): ");
+            String direction = input.next();
+            switch (direction) {
+                case "a":
+                dragon1.moveLeft();
+                break;
+                case "w":
+                dragon1.moveUp();
+                break;
+                case "s":
+                dragon1.moveDown();
+                break;
+                case "d":
+                dragon1.moveRight();
+                break;
+                default:
+                System.out.println("Arah tidak valid!");
+            }
+        }
+    }
+}
