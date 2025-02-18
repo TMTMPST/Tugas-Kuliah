c976f42b4fbe82fc831db8ba52439e0be68225af
Vidi Joshubzky Saviola <103508462+TMTMPST@users.noreply.github.com>
2025-02-19 00:39:12 +0700

    Photos

---
+++ Semester4/WebLanjut/Week2/PWL-2025/app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about() {
+        return 'Vidi Joshubzky Saviola 2341720112';
+        }
+}
--- Semester4/WebLanjut/Week2/PWL-2025/routes/web.php
+++ Semester4/WebLanjut/Week2/PWL-2025/routes/web.php
@@ -1,6 +1,6 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PhotoController;
 
 Route::get('/', function () {
     return "Selamat Datang";
@@ -26,9 +26,9 @@ Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Pos ke - : ' . $postId . ' Komentar ke - : ' . $commentId;
 });
 
-Route::get('/user/profile', function() {
+Route::get('/user/profile', function () {
     //
-    })->name('profile');
+})->name('profile');
 
 Route::middleware(['first', 'second'])->group(function () {
     Route::get('/', function () {
@@ -45,21 +45,45 @@ Route::domain('{account}.example.com')->group(function () {
 });
 
 
-Route::middleware('auth')->group(function () {
-    Route::get('/user', ['UserController'::class, 'index']);
-    Route::get('/post', ['PostController'::class, 'index']);
-    Route::get('/event', ['EventController'::class, 'index']);
-});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/user', [UserController::class, 'index']);
+//     Route::get('/post', [PostController::class, 'index']);
+//     Route::get('/event', [EventController::class, 'index']);
+// });
 
-Route::prefix('admin')->group(function () {
-    Route::get('/user', ['UserController'::class, 'index']);
-    Route::get('/post', ['PostController'::class, 'index']);
-    Route::get('/event', ['EventController'::class, 'index']);
-});
+// Route::prefix('admin')->group(function () {
+//     Route::get('/user', [UserController::class, 'index']);
+//     Route::get('/post', [PostController::class, 'index']);
+//     Route::get('/event', [EventController::class, 'index']);
+// });
 
 Route::redirect('/here', '/there');
 
 Route::view('/welcome', 'welcome');
 Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
 
-Route::get('/hello', ['WelcomeController'::class,'hello']);
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\homeController;
+use App\Http\Controllers\ArticleController;
+
+Route::get('/hello', [WelcomeController::class, 'hello']);
+Route::get('/', [homeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'about']);
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+
+
+
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index',
+    'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create',
+    'store',
+    'update',
+    'destroy'
+]);
